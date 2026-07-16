9d7b46349e7ee9e1b2c1b0ecbcd200ad807ffd45
Kayra Parker-Nance <kayra.parkernance@gmail.com>
2026-07-16 09:44:08 +0200

    Fixed folder set up and test issues

--- tests/simulation/test_simulation.py
+++ tests/simulation/test_simulation.py
@@ -4,7 +4,6 @@ from brainbridge.simulation.simulationClock import SimulationClock
 from brainbridge.simulation.configuration import Configuration
 from brainbridge.simulation.logger import Logger
 from brainbridge.simulation.component import Component
-from tests.simulatorTests.component_tests import FakeComponent
 
 class TestSimulation(unittest.TestCase):
     def test_add_component(self):
@@ -24,3 +23,13 @@ class TestSimulation(unittest.TestCase):
         simulation = Simulation(clock=SimulationClock(), configuration=Configuration(), logger=Logger())
         self.assertEqual(len(simulation.components), 0)
         simulation.update()  # Should not raise any exceptions
+
+class FakeComponent(Component):
+        def __init__(self):
+             self.update_called = False
+        @property
+        def enabled(self) -> bool:
+            return True
+
+        def update(self):
+            self.update_called = True
--- tests/simulatorTests/logger_tests.py
+++
@@ -1,2 +0,0 @@
-import unittest
-from brainbridge.simulation.logger import Logger
